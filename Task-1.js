b4c013ac7d2a9181b2027873004155554f725e85
jesandante007 <jesandante007@gmail.com>
2023-07-26 18:01:05 +0600

    task-1

--- Task-1.js
+++ Task-1.js
@@ -1,1 +1,12 @@
 // Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+
+const reverseString = (str) => {
+    const strArr = str.split("");
+    const strArrReversed = [];
+    for (let i = str.length - 1; i >= 0; i--) {
+      strArrReversed.push(strArr[i]);
+    }
+    return strArrReversed.join("");
+  };
+  
+  console.log(reverseString('hello world'));
